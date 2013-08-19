6d153746ae086ff2f9c3610ecaa8b83772781a82
Imrich Schindler <imrich.schindler@ifana.sk>
2013-08-19 23:18:16 +0200

    single date for SNAPSHOT

--- src/main/java/org/reficio/p2/utils/JarUtils.java
+++ src/main/java/org/reficio/p2/utils/JarUtils.java
@@ -39,132 +39,131 @@ import java.util.zip.ZipOutputStream;
 
 /**
  * @author: Tom Bujok (tom.bujok@gmail.com)
- * <p/>
- * Reficio™ - Reestablish your software!
- * www.reficio.org
+ *          <p/>
+ *          Reficio™ - Reestablish your software! www.reficio.org
  */
 public class JarUtils {
 
-    private static final String SNAPSHOT_POSTFIX = "SNAPSHOT";
+	private static final String SNAPSHOT_POSTFIX = "SNAPSHOT";
+	private static final String SNAPSHOT_TIMESTAMP = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
 
-    public static void adjustOutputVersion(ResolvedArtifact artifact, File inputFile, File outputFile) {
-        if (artifact.isSnapshot()) {
-            Jar jar = null;
-            try {
-                jar = new Jar(inputFile);
-                Manifest manifest = jar.getManifest();
-                Attributes attributes = manifest.getMainAttributes();
-                String version = attributes.getValue(Analyzer.BUNDLE_VERSION);
-                version = tweakVersion(artifact, version);
-                attributes.putValue(Analyzer.BUNDLE_VERSION, version);
-                jar.write(outputFile);
-            } catch (IOException e) {
-                throw new RuntimeException("Cannot open jar " + outputFile);
-            } catch (Exception e) {
-                throw new RuntimeException("Cannot open jar " + outputFile);
-            } finally {
-                if (jar != null) {
-                    jar.close();
-                }
-            }
-        }
-    }
+	public static void adjustOutputVersion(ResolvedArtifact artifact, File inputFile, File outputFile) {
+		if (artifact.isSnapshot()) {
+			Jar jar = null;
+			try {
+				jar = new Jar(inputFile);
+				Manifest manifest = jar.getManifest();
+				Attributes attributes = manifest.getMainAttributes();
+				String version = attributes.getValue(Analyzer.BUNDLE_VERSION);
+				version = tweakVersion(artifact, version);
+				attributes.putValue(Analyzer.BUNDLE_VERSION, version);
+				jar.write(outputFile);
+			} catch (IOException e) {
+				throw new RuntimeException("Cannot open jar " + outputFile);
+			} catch (Exception e) {
+				throw new RuntimeException("Cannot open jar " + outputFile);
+			} finally {
+				if (jar != null) {
+					jar.close();
+				}
+			}
+		}
+	}
 
-    public static void removeSignature(File jar) {
-        File unsignedJar = new File(jar.getParent(), jar.getName() + ".tmp");
-        try {
-            if (unsignedJar.exists()) {
-                FileUtils.deleteQuietly(unsignedJar);
-                unsignedJar = new File(jar.getParent(), jar.getName() + ".tmp");
-            }
-            if (!unsignedJar.createNewFile()) {
-                throw new RuntimeException("Cannot create file " + unsignedJar);
-            }
+	public static void removeSignature(File jar) {
+		File unsignedJar = new File(jar.getParent(), jar.getName() + ".tmp");
+		try {
+			if (unsignedJar.exists()) {
+				FileUtils.deleteQuietly(unsignedJar);
+				unsignedJar = new File(jar.getParent(), jar.getName() + ".tmp");
+			}
+			if (!unsignedJar.createNewFile()) {
+				throw new RuntimeException("Cannot create file " + unsignedJar);
+			}
 
-            ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(unsignedJar));
-            try {
-                ZipFile zip = new ZipFile(jar);
-                for (Enumeration list = zip.entries(); list.hasMoreElements(); ) {
-                    ZipEntry entry = (ZipEntry) list.nextElement();
-                    String name = entry.getName();
-                    if (entry.isDirectory()) {
-                        continue;
-                    } else if (name.endsWith(".RSA") || name.endsWith(".DSA") || name.endsWith(".SF")) {
-                        continue;
-                    }
+			ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(unsignedJar));
+			try {
+				ZipFile zip = new ZipFile(jar);
+				for (Enumeration list = zip.entries(); list.hasMoreElements();) {
+					ZipEntry entry = (ZipEntry) list.nextElement();
+					String name = entry.getName();
+					if (entry.isDirectory()) {
+						continue;
+					} else if (name.endsWith(".RSA") || name.endsWith(".DSA") || name.endsWith(".SF")) {
+						continue;
+					}
 
-                    InputStream zipInputStream = zip.getInputStream(entry);
-                    zipOutputStream.putNextEntry(entry);
-                    try {
-                        IOUtils.copy(zipInputStream, zipOutputStream);
-                    } finally {
-                        zipInputStream.close();
-                    }
-                }
-                IOUtils.closeQuietly(zipOutputStream);
-                FileUtils.copyFile(unsignedJar, jar);
-            } finally {
-                IOUtils.closeQuietly(zipOutputStream);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } finally {
-            FileUtils.deleteQuietly(unsignedJar);
-        }
-    }
+					InputStream zipInputStream = zip.getInputStream(entry);
+					zipOutputStream.putNextEntry(entry);
+					try {
+						IOUtils.copy(zipInputStream, zipOutputStream);
+					} finally {
+						zipInputStream.close();
+					}
+				}
+				IOUtils.closeQuietly(zipOutputStream);
+				FileUtils.copyFile(unsignedJar, jar);
+			} finally {
+				IOUtils.closeQuietly(zipOutputStream);
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		} finally {
+			FileUtils.deleteQuietly(unsignedJar);
+		}
+	}
 
-    public static boolean containsSignature(File jarToUnsign) {
-        try {
-            ZipFile zip = new ZipFile(jarToUnsign);
-            try {
-                for (Enumeration list = zip.entries(); list.hasMoreElements(); ) {
-                    ZipEntry entry = (ZipEntry) list.nextElement();
-                    String name = entry.getName();
-                    if (!entry.isDirectory() && (name.endsWith(".RSA") || name.endsWith(".DSA") || name.endsWith(".SF"))) {
-                        return true;
-                    }
-                }
-                return false;
-            } finally {
-                zip.close();
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+	public static boolean containsSignature(File jarToUnsign) {
+		try {
+			ZipFile zip = new ZipFile(jarToUnsign);
+			try {
+				for (Enumeration list = zip.entries(); list.hasMoreElements();) {
+					ZipEntry entry = (ZipEntry) list.nextElement();
+					String name = entry.getName();
+					if (!entry.isDirectory() && (name.endsWith(".RSA") || name.endsWith(".DSA") || name.endsWith(".SF"))) {
+						return true;
+					}
+				}
+				return false;
+			} finally {
+				zip.close();
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-    private static String getTimeStamp() {
-        SimpleDateFormat format = new SimpleDateFormat("yyyyMMddHHmmss");
-        return format.format(new Date());
-    }
+	private static String getTimeStamp() {
+		return SNAPSHOT_TIMESTAMP;
+	}
 
-    public static String tweakVersion(ResolvedArtifact artifact, String version) {
-        String tweakedVersion = version;
-        if (version.contains(SNAPSHOT_POSTFIX)) {
-            String postfix = getSnapshotPostfix(artifact);
-            if (StringUtils.isBlank(postfix)) {
-                postfix = getTimeStamp();
-            }
-            tweakedVersion = tweakedVersion.replace("-" + SNAPSHOT_POSTFIX, "." + postfix);
-            tweakedVersion = tweakedVersion.replace("." + SNAPSHOT_POSTFIX, "." + postfix);
-        }
-        return tweakedVersion;
-    }
+	public static String tweakVersion(ResolvedArtifact artifact, String version) {
+		String tweakedVersion = version;
+		if (version.contains(SNAPSHOT_POSTFIX)) {
+			String postfix = getSnapshotPostfix(artifact);
+			if (StringUtils.isBlank(postfix)) {
+				postfix = getTimeStamp();
+			}
+			tweakedVersion = tweakedVersion.replace("-" + SNAPSHOT_POSTFIX, "." + postfix);
+			tweakedVersion = tweakedVersion.replace("." + SNAPSHOT_POSTFIX, "." + postfix);
+		}
+		return tweakedVersion;
+	}
 
-    private static String getSnapshotPostfix(ResolvedArtifact artifact) {
-        if (artifact.isSnapshot()) {
-            String version = artifact.getArtifact().getVersion();
-            int dashIndex = version.indexOf("-");
-            if (dashIndex > 0) {
-                String postfix = version.substring(dashIndex + 1);
-                postfix = postfix.replace(".", "");
-                if (postfix.contains(SNAPSHOT_POSTFIX)) {
-                    return "";
-                }
-                return postfix;
-            }
-        }
-        return "";
-    }
+	private static String getSnapshotPostfix(ResolvedArtifact artifact) {
+		if (artifact.isSnapshot()) {
+			String version = artifact.getArtifact().getVersion();
+			int dashIndex = version.indexOf("-");
+			if (dashIndex > 0) {
+				String postfix = version.substring(dashIndex + 1);
+				postfix = postfix.replace(".", "");
+				if (postfix.contains(SNAPSHOT_POSTFIX)) {
+					return "";
+				}
+				return postfix;
+			}
+		}
+		return "";
+	}
 
 }
